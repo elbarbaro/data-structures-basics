5d847a28fd4772718c44d5c1b49dbb4a4c8a07ee
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2022-07-23 08:48:47 -0500

    initial commit

---
+++ groceries_list.py
@@ -0,0 +1,16 @@
+# Algoritmo que pregunta 10 frutas o verduras 
+# y las almacena en lo que es nuestra canaste del mandado
+# Se pueden repertir
+# Ejemplo: Manzana, Uvas, uvas, Chayote, papa
+
+MAX_ITEMS = 10
+groceries = []
+
+for count in range(MAX_ITEMS):
+	item = input(f'Objeto {count + 1} de la lista: ')
+	# Métodos de manejo de strings
+	item_lower = item.lower()
+	groceries.append(item_lower)
+
+print('Tu mandado quedó: ')
+print(groceries)
